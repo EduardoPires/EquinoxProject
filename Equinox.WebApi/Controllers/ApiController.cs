293580a51e82a5beafd94963b5e7d603228507ea
Thiago Lunardi <t.lunardi@kapilendo.de>
2017-08-03 15:45:14 +0200

    Added project with WebAPI without Mvc

---
+++ Equinox.WebApi/Controllers/ApiController.cs
@@ -0,0 +1,23 @@
+﻿using Equinox.Domain.Core.Notifications;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Equinox.WebApi.Controllers
+{
+    public abstract class ApiController : ControllerBase
+    {
+        private readonly IDomainNotificationHandler<DomainNotification> _notifications;
+
+        public ApiController(IDomainNotificationHandler<DomainNotification> notifications)
+        {
+            _notifications = notifications;
+        }
+
+        protected IEnumerable<DomainNotification> Notifications => _notifications.GetNotifications();
+
+        protected bool IsValidOperation()
+        {
+            return (!_notifications.HasNotifications());
+        }
+    }
+}
