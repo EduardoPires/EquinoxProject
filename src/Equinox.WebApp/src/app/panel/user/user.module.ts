48ff5263f7c2998034f0cc60511e23e0871167cd
Bruno Brito <bhdebrito@gmail.com>
2018-03-29 17:43:37 -0300

    daily commit

--- src/Equinox.WebApp/src/app/panel/user/user.module.ts
+++ src/Equinox.WebApp/src/app/panel/user/user.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from "@angular/router";
 import { SharedModule } from "../../shared/shared.module";
 import { DndModule } from "ng2-dnd";
 import { ProfileComponent } from "./profile/profile.component";
+import { TreeModule } from "angular-tree-component";
+import { AgmCoreModule } from "@agm/core";
+import { NgxSelectModule } from "ngx-select-ex";
 
 const routes: Routes = [
     { path: "", redirectTo: "profile", pathMatch: "full" },
@@ -14,6 +17,11 @@ const routes: Routes = [
         SharedModule,
         RouterModule.forChild(routes),
         DndModule.forRoot(),
+        TreeModule,
+        AgmCoreModule.forRoot({
+            apiKey: "AIzaSyBNs42Rt_CyxAqdbIBK0a5Ut83QiauESPA"
+        }),
+        NgxSelectModule
     ],
     declarations: [
         ProfileComponent
