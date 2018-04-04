f3c37160d176c70ffd098bdbedcc50a914a34dab
Bruno Brito <bhdebrito@gmail.com>
2018-04-04 17:45:56 -0300

    ConfirmEmail

---
+++ src/Equinox.Infra.CrossCutting.Identity/Models/AccountViewModels/ConfirmEmailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Equinox.Infra.CrossCutting.Identity.Models.AccountViewModels
+{
+    public class ConfirmEmailViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Code { get; set; }
+    }
+}
