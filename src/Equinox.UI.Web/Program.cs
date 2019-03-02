83dabafb83e8485f7f8801d473fa69b01e5bab99
Moien Tajik <moientajik@yahoo.com>
2019-03-02 21:52:25 +0330

    Removed unused usings in project files

--- src/Equinox.UI.Web/Program.cs
+++ src/Equinox.UI.Web/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Equinox.UI.Web
 {
@@ -17,7 +10,7 @@ namespace Equinox.UI.Web
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
