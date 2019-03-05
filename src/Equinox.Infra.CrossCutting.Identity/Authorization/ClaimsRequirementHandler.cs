474ac9bd4f74f13a05c10203739b25547b88c97f
Kaue Reinbold <ck_reinbold@hotmail.com>
2019-03-04 21:55:36 -0300

    Validating if user has rights based on claims.

--- src/Equinox.Infra.CrossCutting.Identity/Authorization/ClaimsRequirementHandler.cs
+++ src/Equinox.Infra.CrossCutting.Identity/Authorization/ClaimsRequirementHandler.cs
@@ -10,13 +10,11 @@ namespace Equinox.Infra.CrossCutting.Identity.Authorization
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
                                                        ClaimRequirement requirement)
         {
-
-            var claim = context.User.Claims.FirstOrDefault(c => c.Type == requirement.ClaimName);
-            if (claim != null && claim.Value.Contains(requirement.ClaimValue))
+            if (context.User.Claims.Any(c => c.Value.Contains(requirement.ClaimValue)))
             {
                 context.Succeed(requirement);
             }
-
+            
             return Task.CompletedTask;
         }
     }
