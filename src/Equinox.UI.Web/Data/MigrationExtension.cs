0a6ad803c68859c7b1bd38f8dea5033402588344
doks-coders <guonnie@gmail.com>
2024-03-19 23:25:00 +0100

    Add migrate feature
    - Implement functionality to run initial migration before the application starts
    
    Fixes #199

---
+++ src/Equinox.UI.Web/Data/MigrationExtension.cs
@@ -0,0 +1,38 @@
+﻿using Equinox.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Equinox.UI.Web.Data
+{
+    public static class MigrationsExtension
+    {
+        /// <summary>
+        /// This contains the migration code for the application
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns>A reference to app after the operation has completed</returns>
+        public static async Task<WebApplication> Migrate(this WebApplication app)
+        {
+            using (var program = app.Services.CreateScope())
+            {
+                var service = program.ServiceProvider;
+
+                var logger = service.GetService<ILogger<Program>>();
+                try
+                {
+                    var equinoxContext = service.GetRequiredService<EquinoxContext>();
+                    var eventStoreSqlContext = service.GetRequiredService<EventStoreSqlContext>();
+
+                    await equinoxContext.Database.MigrateAsync();
+                    await eventStoreSqlContext.Database.MigrateAsync();
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex.Message, ex);
+
+                }
+            }
+            return app;
+        }
+    }
+
+}
