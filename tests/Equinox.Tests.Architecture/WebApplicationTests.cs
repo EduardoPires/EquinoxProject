e83638c96c621a8dac1de18ca1f8b35e9b880e1c
Eduardo Pires <contato@eduardopires.net.br>
2024-08-02 13:39:37 -0300

    Improvements

--- tests/Equinox.Tests.Architecture/WebApplicationTests.cs
+++ tests/Equinox.Tests.Architecture/WebApplicationTests.cs
@@ -1,15 +1,5 @@
-﻿using Equinox.Application.Services;
-using Equinox.Domain.Core.Events;
-using Equinox.Domain.Models;
-using Equinox.Infra.CrossCutting.Bus;
-using Equinox.Infra.CrossCutting.Identity.User;
-using Equinox.Infra.CrossCutting.IoC;
-using Equinox.Infra.Data.Context;
-using Equinox.Services.Api.Controllers;
-using Equinox.UI.Web.Controllers;
+﻿using Equinox.UI.Web.Controllers;
 using NetArchTest.Rules;
-using NetArchTest.Rules.Policies;
-using System.Reflection;
 
 namespace Equinox.Tests.Architecture;
 
